ebf677da67b5ea46c67c0bcf2b1f33f715877e45
Durgé Seerden <dsdeur@gmail.com>
2015-03-09 12:21:21 +0100

    added main scene

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
 var Scene = require("./scene.js");
 
-Scene.init();
+var scene = new Scene(document.body);
+scene.resize(window.innerWidth, window.innerHeight);
+scene.render();
+
+window.onresize = function() {
+    scene.resize(window.innerWidth, window.innerHeight);
+}
--- src/js/scene.js
+++ src/js/scene.js
@@ -1,5 +1,47 @@
-module.exports = {
-    init: function() {
-        console.log("hello world");
+var THREE = require('three');
+
+module.exports = function(element) {
+    var self = this;
+    this.element = element;
+
+    this.init = function(element) {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 10000);
+        this.renderer = new THREE.WebGLRenderer();
+
+        this.renderer.shadowMapEnabled = true;
+        this.element.appendChild(this.renderer.domElement);
+
+        this.ambient = new THREE.AmbientLight(0x999999);
+        this.light = new THREE.DirectionalLight(0x999999);
+
+        this.light.castShadow = true;
+        this.light.shadowMapWidth = 2048;
+        this.light.shadowMapHeight = 2048;
+        this.light.shadowCameraLeft = -500;
+        this.light.shadowCameraRight = 500;
+        this.light.shadowCameraTop = 500;
+        this.light.shadowCameraBottom = -500;
+        this.light.shadowCameraFar = 3500;
+
+        this.camera.position.y = 100;
+        this.camera.position.z = 300;
+
+        this.scene.add(this.ambient);
+        this.scene.add(this.light);
     }
+
+    this.resize = function(width, height) {
+        this.camera.aspect = width/height;
+        this.renderer.setSize(width, height);
+        this.camera.updateProjectionMatrix();
+    },
+
+    this.render = function() {
+        requestAnimationFrame(self.render);
+
+        self.renderer.render(self.scene, self.camera);
+    }
+
+    this.init(element);
 }
