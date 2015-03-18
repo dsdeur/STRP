bc9ecbdd77386ab988b4326b87bead322970f3ae
Durgé Seerden <dsdeur@gmail.com>
2015-03-18 18:14:59 +0100

    support hex color input

--- src/js/shaders/transform_normal_uniforms.js
+++ src/js/shaders/transform_normal_uniforms.js
@@ -1,12 +1,25 @@
 var THREE = require('three');
 
-function rgbToVector(rgb) {
+function hexToVector(hex) {
+    rgb = hexToRgb(hex);
+
     var r = rgb[0]/255;
     var g = rgb[1]/255;
     var b = rgb[2]/255;
 
     return new THREE.Vector3(r,g,b);
 }
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+    return rgb = [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ];
+}
+
 
 module.exports = function(config) {
     this.bt = {
@@ -41,6 +54,6 @@ module.exports = function(config) {
 
     this.color = {
         type:"v3",
-        value: rgbToVector(config.color)
+        value: hexToVector('#' + config.color)
     }
 };
